e0c783659f17792dd2ea46a9ca0d8bcaaddf8534
Sayed Alwedaie <5216479+sydalwedaie@users.noreply.github.com>
2024-09-19 16:24:11 +0600

    Create basic branch

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,14 +1,11 @@
 import { resolve } from "path";
-import { data } from "./src/data/index.js";
+import { data } from "./src/data.js";
 import handlebars from "vite-plugin-handlebars";
 
 export default {
   plugins: [
     handlebars({
-      partialDirectory: [
-        resolve(__dirname, "src/layouts"),
-        resolve(__dirname, "src/components"),
-      ],
+      partialDirectory: resolve(__dirname, "./src/components"),
       context: data,
     }),
   ],
